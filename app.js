0eab4b23973b8442a6f9ab35f4d861ad0e307f46
Musa Talluzi <musa.talluzi@gmail.com>
2018-08-25 10:10:12 -0400

    Implement users rounter using passport and mongoose sessions

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var session = require('express-session');
+var passport = require('passport');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -16,19 +18,39 @@ app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+app.use(session({
+  name: 'session-id',
+  secret: '12345-67890-09876-54320',
+  saveUninitialized: false,
+  resave: false,
+  store: new FileStore()
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use('/users', usersRouter);
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
+const mongoose = require('mongoose');
+mongoose.Promise = require('bluebird');
+
+const url = 'mongodb://localhost:27017/conFusion';
+const connect = mongoose.connect(url);
+connect.then((client) => {
+  console.log('Connected correctly to server');
+})
+  .catch((err) => console.log(err));
+
+
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
