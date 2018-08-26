5bfcb3268eacbad7700e35ce0d46f205bcc7b551
Musa Talluzi <musa.talluzi@gmail.com>
2018-08-26 02:44:35 -0400

    fix users.js

--- routes/users.js
+++ routes/users.js
@@ -1,79 +1,9 @@
 var express = require('express');
 var router = express.Router();
-<<<<<<< HEAD
-const bodyParser = require('body-parser');
-
-const User = require('../models/user');
-
-router.use(bodyParser.json());
-
-/* GET users listing. */
-router.get('/', function (req, res, next) {
-  User.find(req.query)
-    .populate('reporter')
-    .then((users) => {
-      res.statusCode = 200;
-      res.setHeader('Content-Type', 'application/json');
-      res.json(users);
-    }, (err) => next(err))
-    .catch((err) => next(err));
-});
-
-router.post('/signup', (req, res, next) => {
-  User.register(new User({ username: req.body.username }),
-    req.body.password, (err, user) => {
-      if (err) {
-        res.statusCode = 500;
-        res.setHeader('Content-Type', 'application/json');
-        res.json({ err: err });
-      }
-      if (req.body.firstname) {
-        user.firstname = req.body.firstname;
-      }
-      if (req.body.lastname) {
-        user.lastname = req.body.lastname;
-      }
-      user.save((err, user) => {
-        if (err) {
-          res.statusCode = 500;
-          res.setHeader('Content-Type', 'application/json');
-          res.json({ err: err });
-          return;
-        }
-        passport.authenticate('local')(req, res, () => {
-          res.statusCode = 200;
-          res.setHeader('Content-Type', 'application/json');
-          res.json({ success: true, status: 'Registration Successful' });
-        });
-      });
-    });
-});
-
-router.post('/login', passport.authenticate('local'), (req, res) => {
-  console.log(req.body);
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'application/json');
-  res.json({ success: true, status: 'You are successfully logged in!' });
-});
-
-router.get('/logout', (req, res, next) => {
-  if (req.session) {
-    req.session.destroy();
-    res.clearCookie('session-id');
-    res.redirect('/');
-  } else {
-    var err = new Error('You are not logged in!');
-    err.status = 403;
-    next(err);
-  }
-});
-
-=======
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
->>>>>>> tmp-work
 module.exports = router;
